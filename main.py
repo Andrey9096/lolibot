c90c9054855f5217140337af817a90e2af44623f
Andrey9096 <dota2woodo@gmail.com>
2021-06-21 17:36:11 +1000

    first commit

---
+++ main.py
@@ -0,0 +1,21 @@
+import logging
+from aiogram import Bot, Dispatcher, executor, types
+
+API_TOKEN = '1819449909:AAGXFeRV4KulEdSO4fEMZQxq2M6EoAta-qg'
+logging.basicConfig(level=logging.INFO)
+
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot)
+
+@dp.message_handler(commands=['start'])
+async def send_msg(message: types.Message):
+    await message.reply("Hello")
+
+@dp.message_handler(content_types=["new_chat_members"])
+async def new_member(message):
+    user_name = message.new_chat_member.first_name
+    bot.send_message(message.chat.id, "Добро пожаловать, {0}! Правило одно - не мамкоебить!".format(user_name))
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
