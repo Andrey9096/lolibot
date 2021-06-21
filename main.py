79d4393367e0b943c823a37e97eafc9b7e9f06e5
Andrey9096 <dota2woodo@gmail.com>
2021-06-21 20:11:32 +1000

    UPD

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import logging
 from aiogram import Bot, Dispatcher, executor, types
-
+from aiogram.dispatcher.filters import BoundFilter
 API_TOKEN = '1819449909:AAGXFeRV4KulEdSO4fEMZQxq2M6EoAta-qg'
 logging.basicConfig(level=logging.INFO)
 
@@ -15,6 +15,26 @@ async def send_msg(message: types.Message):
 async def new_member(message):
     user_name = message.new_chat_members[0].first_name
     await  bot.send_message(message.chat.id, "Добро пожаловать, {0}! Правило одно - не мамкоебить!".format(user_name))
+
+
+@dp.message_handler(commands=['rights'])
+async def myrights(message: types.Message):
+    member = await bot.get_chat_member(message.chat.id, API_TOKEN.split(":")[0])
+
+
+
+
+#async def my_filter(message: types.Message):
+#    return {'растер' : 'пидар'}
+#
+#
+#
+#@dp.message_handler(my_filter)
+#async def my_message_handler(message: types.Message):
+#    await message.reply()
+#
+
+
 
 
 if __name__ == '__main__':
